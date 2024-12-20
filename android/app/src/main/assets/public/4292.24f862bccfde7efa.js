b230d3668a0d32f18507528d31437249456a7014
Pato <patricio62014@gmail.com>
2024-12-19 23:51:08 -0300

    Entrega Avance
    exportado a Android, con sus funcionalidades correspondientes y solicitadas

---
+++ android/app/src/main/assets/public/4292.24f862bccfde7efa.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4292],{4292:(d,n,e)=>{e.r(n),e.d(n,{SplashScreenWeb:()=>_});var s=e(467),o=e(5083);class _ extends o.E_{show(u){return(0,s.A)(function*(){})()}hide(u){return(0,s.A)(function*(){})()}}}}]);
